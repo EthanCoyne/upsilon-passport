dea63afe9f4896596e1ca79490c5576d2233034e
Ethan Coyne <ethancoyne@Ethans-MacBook-Pro.local>
2017-02-08 14:30:50 -0600

    added registration form

---
+++ public/scripts/register.controller.js
@@ -0,0 +1,18 @@
+angular.module('passportApp').controller('RegisterController', function($http, $location) {
+
+    var ctrl = this;
+
+    ctrl.register = function() {
+      console.log('registering');
+      $http.post('/register', {
+        username: ctrl.username,
+        password: ctrl.password
+      }).then(function(response){
+        console.log(response);
+        $location.path('/home');
+      }, function(error) {
+        console.log('error registering in', error);
+      });
+    };
+
+}); // end RegisterController
